ba90ecc3a886fad86550f7057a0bb9bab82002fe
Supharoek <ueki2542@gmail.com>
2021-11-16 22:41:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ from keras.models import load_model
 
 app = Flask(__name__)
 
-#os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'serviceAccount.json'
-
 classes = ["AluCan", "Glass", "PET"]
+model = load_model('model_70_15_15_b256_v5.h5')
 
 @app.route('/api/predict/', methods=['GET', 'POST'])
 def predict():
@@ -23,12 +22,11 @@ def predict():
         # image = tf.cast(tf.expand_dims(nparr, axis=0), tf.int16)
         # print(nparr)
         # instances_list = tf.cast(nparr, tf.float16).numpy().tolist()
-        #image.numpy().tolist()
+        # image.numpy().tolist()
         print(nparr.shape)
         prediction = model.predict(nparr)
         predict_class = classes[tf.argmax(prediction[0])]
         return jsonify(predict_class)
 
 if __name__ == '__main__':
-    model = load_model('model_70_15_15_b256_v5.h5')
-    app.run(debug=True)
+    app.run()
